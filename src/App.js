6e16fffec08bb2a244e5b480cee1466a36c8247b
priya <riaharshini1998@gmail.com>
2021-01-31 15:57:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from '@material-ui/core/Card';
+import Bio from "./sample";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Card className="card" variant="outlined">
+      <img src="/asserts/picture.png" className="img2"  alt="profile" width="170" height="170"/>
+     <Bio></Bio>
+    </Card>
   );
 }
 
---
+++ src/sample.js
@@ -0,0 +1,15 @@
+import React from "react";
+function Bio(){
+    return(
+        <pre> 
+        NAME: PriyaDharshini<br/>
+        
+        EDUCATION: MSC computerscience<br/>
+
+        EMAIL:riaharshini1998@gmail.com<br/>
+        
+        SKILLS: TeamWork,ProblemSolving
+        </pre>
+    );
+}
+export default Bio
